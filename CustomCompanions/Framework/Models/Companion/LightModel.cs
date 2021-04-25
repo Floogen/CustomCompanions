67af24dc17a6c63b33ddf36db0c4177c7b6efe38
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-04-25 15:58:25 -0500

    Added OffsetX and OffsetY as optional parameters to LightModel

--- CustomCompanions/Framework/Models/Companion/LightModel.cs
+++ CustomCompanions/Framework/Models/Companion/LightModel.cs
@@ -4,12 +4,14 @@
     {
         public int[] Color { get; set; }
         public float Radius { get; set; }
+        public float OffsetX { get; set; }
+        public float OffsetY { get; set; }
         public float PulseInterval { get; set; }
         public int PulseSpeed { get; set; }
 
         public override string ToString()
         {
-            return $"[Color: {string.Join(",", Color)} | Radius: {Radius} | PulseInterval: {PulseInterval} | PulseSpeed: {PulseSpeed}]";
+            return $"[Color: {string.Join(",", Color)} | Radius: {Radius} | Offset: {OffsetX}, {OffsetY} | PulseInterval: {PulseInterval} | PulseSpeed: {PulseSpeed}]";
         }
     }
 }
