6a51a2e3b2559e5999244dfa330b48756dd7b342
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-07-20 14:04:17 -0500

    Set default FrameSizeWidth and FrameSizeHeight to 64 to match game default

--- CustomCompanions/Framework/Models/Companion/PortraitModel.cs
+++ CustomCompanions/Framework/Models/Companion/PortraitModel.cs
@@ -3,8 +3,8 @@
     public class PortraitModel
     {
         public string PortraitSheetPath { get; set; }
-        public int FrameSizeWidth { get; set; }
-        public int FrameSizeHeight { get; set; }
+        public int FrameSizeWidth { get; set; } = 64;
+        public int FrameSizeHeight { get; set; } = 64;
         public int FrameIndex { get; set; }
         public string PortraitDisplayName { get; set; }
 
