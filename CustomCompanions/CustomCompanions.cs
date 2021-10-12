0d5ba41c7fc72cb4abcf2ddbfa675a01986ccbb9
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-10-11 19:58:07 -0500

    Implemented CompanionModel.EnableSpawnAtDayStart property

--- CustomCompanions/CustomCompanions.cs
+++ CustomCompanions/CustomCompanions.cs
@@ -159,6 +159,12 @@ namespace CustomCompanions
 
             // Check for any Content Patcher changes from OnDayStart
             this.ValidateModelCache(null, true, true);
+
+            // Spawn any required companions
+            foreach (var location in Game1.locations)
+            {
+                this.SpawnSceneryCompanions(location, spawnOnlyRequiredCompanions: true);
+            }
         }
 
         private void OnCustomLoad(object sender, EventArgs e)
@@ -417,7 +423,7 @@ namespace CustomCompanions
             CompanionManager.sceneryCompanions = new List<SceneryCompanions>();
         }
 
-        private void SpawnSceneryCompanions(GameLocation location)
+        private void SpawnSceneryCompanions(GameLocation location, bool spawnOnlyRequiredCompanions = false)
         {
             var backLayer = location.map.GetLayer("Back");
             for (int x = 0; x < backLayer.LayerWidth; x++)
@@ -463,6 +469,11 @@ namespace CustomCompanions
                         if (companion is null)
                         {
                             Monitor.Log($"Unable to find companion match for {companionKey} given on tile ({x}, {y}) for map {location.NameOrUniqueName}!", LogLevel.Warn);
+                            continue;
+                        }
+
+                        if (spawnOnlyRequiredCompanions && !companion.EnableSpawnAtDayStart)
+                        {
                             continue;
                         }
 
